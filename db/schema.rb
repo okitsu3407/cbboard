cc0aa1761ab0615baf9817ed77b42713b38a1832
3411 <3411@example.com>
2020-10-07 11:08:46 +0900

    競合解決

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200925060606) do
+ActiveRecord::Schema.define(version: 20201005032323) do
+
+  create_table "subjects", force: :cascade do |t|
+    t.string "title"
+    t.string "classroom"
+    t.string "teacher"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "timetables", force: :cascade do |t|
     t.integer "grade_id"
