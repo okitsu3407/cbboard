4621720a66169f6aab6e9ba41759950d4c0c9b8b
3407 <2020ibhs.3407@gmail.com>
2020-10-07 12:16:29 +0900

    'マスター作成１'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Subject.delete_all
+Subject.connection.execute("delete from sqlite_sequence where name='subjects'")
+Subject.create(title: "")
+Subject.create(title: "")
+Subject.create(title: "")
+Subject.create(title: "")
+Subject.create(title: "")
+
+Age.delete_all
+Age.connection.execute("delete from sqlite_sequence where name='ages'")
+Age.create(name: "18", rank: 18)
+Age.create(name: "19", rank: 19)
+
+Age.delete_all
+Age.connection.execute("delete from sqlite_sequence where name='ages'")
+Age.create(name: "18", rank: 18)
+Age.create(name: "19", rank: 19)
