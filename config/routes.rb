a562f04231c5034e80e83d1e5bb7822e054a747e
3407 <2020ibhs.3407@gmail.com>
2020-10-07 12:00:41 +0900

    'topページ作成完了'

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   get 'home/top'
 
-=======
   resources :gclasses
   resources :subjects
->>>>>>> 0a414d9fbfa28c89b174975fc97e7f0b24b20c4d
   resources :grates
   resources :users
   resources :timetables
