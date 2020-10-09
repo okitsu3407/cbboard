f627d7a7aafa75b9fecda8db2fcaa13b9e86ccc1
3419 <3419@example.com>
2020-10-09 13:53:54 +0900

    リレーション

--- app/models/timetable.rb
+++ app/models/timetable.rb
@@ -1,6 +1,10 @@
 class Timetable < ApplicationRecord
-
+ 
+    belongs_to :user
+    belongs_to :title
     belongs_to :grate
     belongs_to :gclass
-    
+    belongs_to :teacher
+    belongs_to :classroom
+
 end
