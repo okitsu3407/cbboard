241684e01cc37e53dbf76a4c5c81a0bd3da23be4
3407 <2020ibhs.3407@gmail.com>
2020-11-04 12:22:10 +0900

    a

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,13 @@
 class HomeController < ApplicationController
+
+    
+
+    def top
+        @today = Date.today
+        week = ['日','月','火','水','木','金','土'] 
+        @yobi = week[@today.wday]
+    end
+
+   
+
 end
