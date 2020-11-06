c48a31890121f6603e7757f94b3f98d24cb2eb0d
3411 <3411@example.com>
2020-11-06 11:40:53 +0900

    g

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -24,10 +24,4 @@ class HomeController < ApplicationController
         @yobi = week[@today.wday]
     end
 
-    def create
-        if session[:login] = true
-            flash.now[:alert] = "ようこそ　名無しさん"
-        elsif session[:login] = ""
-            flash.now[:alert] = ""
-
 end
