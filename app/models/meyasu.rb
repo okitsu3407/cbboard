f91a86cd24a19d37c76f129b730f802589fabd0a
3419 <3419@example.com>
2020-11-20 15:04:45 +0900

    入力チェック

--- app/models/meyasu.rb
+++ app/models/meyasu.rb
@@ -1,2 +1,18 @@
 class Meyasu < ApplicationRecord
+
+    validate :error_check
+
+    def error_check
+
+        @out = "ああああ"
+
+        if content.blank?
+            errors[:base] << '必ず何かを入力してください'
+        end
+
+        if content.include?(@out)
+            errors[:base] << 'あなたが入力した文に不適切な内容が含まれています'
+        end
+
+    end
 end
