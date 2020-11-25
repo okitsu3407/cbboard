71d2378c12b48e44dc01ea5fb5c1cef8e1260e9e
3419 <3419@example.com>
2020-11-25 12:01:28 +0900

    目安箱

--- app/models/meyasu.rb
+++ app/models/meyasu.rb
@@ -2,17 +2,27 @@ class Meyasu < ApplicationRecord
 
     validate :error_check
 
-    def error_check
 
-        @out = "ああああ"
+def error_check
 
-        if content.blank?
-            errors[:base] << '必ず何かを入力してください'
+    @out = ["ああああ","かかかか","ささささ","たたたた","なななな","はははは"]
+    n = 0
+
+    if content.blank?
+        errors[:base] << '必ず何かを入力してください'
+    end       
+
+    @out.each do |i|
+        if content.include?(i)
+            n += 1
         end
-
-        if content.include?(@out)
-            errors[:base] << 'あなたが入力した文に不適切な内容が含まれています'
-        end
-
     end
+
+    if n >= 1
+    errors[:base] << 'あなたが入力した文に不適切な内容が含まれています'
+    errors[:base] << '入力をやり直してください'
+    end
+
+end
+
 end
