9f3657a75bcdc00a38c8524a3b1d763aaf22e5cb
3407 <2020ibhs.3407@gmail.com>
2020-12-11 13:26:58 +0900

    入力チェック

--- app/models/timetable.rb
+++ app/models/timetable.rb
@@ -1,11 +1,30 @@
 class Timetable < ApplicationRecord
  
     belongs_to :user,optional: true
-    belongs_to :title
+    belongs_to :title,optional: true
     belongs_to :grate
     belongs_to :gclass
-    belongs_to :teacher
-    belongs_to :classroom
+    belongs_to :teacher,optional: true
+    belongs_to :classroom,optional: true
     belongs_to :clock
+
+
+   validate :error_check
+
+    def error_check
+
+        if title_id == 0
+            errors[:base] << '教科を選択してください'
+        end
+        
+        if classroom_id == 0
+            errors[:base] << '教室を選択してください'
+        end
+
+        if teacher_id == 0
+            errors[:base] << '先生を選択してください'
+        end
+
+    end
 
 end
