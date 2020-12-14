f8be677ccc579de96249e762f0cba3f4914b85e7
3419 <3419@example.com>
2020-12-14 12:21:39 +0900

    meyasu

--- app/controllers/meyasus_controller.rb
+++ app/controllers/meyasus_controller.rb
@@ -26,6 +26,7 @@ class MeyasusController < ApplicationController
   def create
     @meyasu = Meyasu.new
     @meyasu.content = params[:freeans]
+    @meyasu.grate  = params[:gakunen]
 
     respond_to do |format|
       if @meyasu.save
