070f62e65ffe3f76e10c67d713a5567bc88c1e21
3419 <3419@example.com>
2020-12-18 13:59:25 +0900

    調整

--- app/controllers/meyasus_controller.rb
+++ app/controllers/meyasus_controller.rb
@@ -26,7 +26,6 @@ class MeyasusController < ApplicationController
   def create
     @meyasu = Meyasu.new
     @meyasu.content = params[:freeans]
-    Meyasu.order(grate: :DESC)
     @meyasu.grate  = params[:gakunen]
 
     respond_to do |format|
--- app/models/meyasu.rb
+++ app/models/meyasu.rb
@@ -5,12 +5,26 @@ class Meyasu < ApplicationRecord
 
 def error_check
 
-    @out = ["fuck you","死ね","アホ","あほ","馬鹿","バカ","キモイ","きもい","キモ","デブ","ブス","うざい"]
+    @out  = ["fuck you","死ね","アホ","あほ","馬鹿","バカ","キモイ","きもい","キモ","デブ","ブス","うざい"]
+    @out2 = ["a","b","c","d","e","f","g","h","i","j","k","m","n","l","o","p","q","r","s","t","u","v","w","x","y","z"]
     n = 0
+    m = 0
 
     if content.blank?
         errors[:base] << '必ず何かを入力してください'
-    end 
+    end
+
+    @b = []
+    @out2.each do |i|
+        if content.include?(i)
+            @b.push i
+            m += 1
+        end
+    end
+
+    if m >= 1
+        errors[:base] << '全角で入力してください'
+    end
 
     @a = []
     @out.each do |i|
